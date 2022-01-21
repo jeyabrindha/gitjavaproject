bc4769e5279a73452dbb281e2843033ae45f0c2b
jeyabrindha <jeyabrindha.it26@gmail.com>
2022-01-21 14:38:25 -0800

    Conflict Resolve

--- src/org/add/Arithmetic.java
+++ src/org/add/Arithmetic.java
@@ -4,7 +4,12 @@ public class Arithmetic {
 	public static void sub() {
 		System.out.println("Subtraction");
 	}
+	public static void add() {
+		System.out.println("Addition");
+	}
 	public static void main(String[] args) {
 		sub();
+		add();
 	}
+	
 }
