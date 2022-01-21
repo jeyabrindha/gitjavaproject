677cce4bdc44654c770af5c8e00ede483e536d4b
jeyabrindha <jeyabrindha.it26@gmail.com>
2022-01-21 14:42:09 -0800

    Conflict Resolve

--- src/org/add/Arithmetic.java
+++ src/org/add/Arithmetic.java
@@ -3,13 +3,10 @@ package org.add;
 public class Arithmetic {
 	public static void sub() {
 		System.out.println("Subtraction");
-	}
-	public static void add() {
 		System.out.println("Addition");
 	}
 	public static void main(String[] args) {
 		sub();
-		add();
 	}
 	
 }
